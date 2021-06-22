5973f2b0b7f8e4240c756c2fe21fb59746a3adcc
silverdes <ouaiji04@gmail.com>
2021-06-22 20:31:03 +0100

    Setting up database tables

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Property extends Model
 {
     use HasFactory;
+
+    protected $fillable=[
+        'estate_name',
+        'city_id',
+        'estate_type_id',
+        'floor_space',
+        'number_of_balconies',
+        'balconies_space',
+        'number_of_bedrooms',
+        'number_of_bathrooms',
+        'number_of_garages',
+        'number_of_parking_spaces',
+        'pets_allowed',
+        'estate_description',
+        'estate_status_id',
+    ];
 }
--- database/migrations/2021_06_22_115348_create_properties_table.php
+++ database/migrations/2021_06_22_115348_create_properties_table.php
@@ -15,6 +15,17 @@ class CreatePropertiesTable extends Migration
     {
         Schema::create('properties', function (Blueprint $table) {
             $table->id();
+            $table->string('estate_name')->nullable();
+            $table->foreignId('city_id')->constrained('cities')->onDelete('cascade');
+            $table->foreignId('estate_type_id')->constrained('estate_types');
+            $table->double('floor_space', 8, 2)->nullable();
+            $table->integer('number_of_balconies')->nullable();
+            $table->double('balconies_space', 8, 2)->nullable();
+            $table->integer('number_of_bedrooms')->nullable();
+            $table->integer('number_of_bathooms')->nullable();
+            $table->integer('number_of_garages')->nullable();
+            $table->integer('number_of_parking_spaces')->nullable();
+            $table->boolean('pets_allowed')->default(0);
             $table->timestamps();
         });
     }
